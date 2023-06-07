19a41459ddcd7f50282d573312bea2c4635a214a
Collin Togher <collin@synadia.com>
2023-06-06 22:42:24 -0400

    disabled unstable APIS, removed uneccesary files

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,11 +4,10 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[room].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/api/login.ts";
-import * as $3 from "./routes/api/logout.ts";
-import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/new.tsx";
+import * as $1 from "./routes/api/login.ts";
+import * as $2 from "./routes/api/logout.ts";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/new.tsx";
 import * as $$0 from "./islands/AddRoom.tsx";
 import * as $$1 from "./islands/Chat.tsx";
 import * as $$2 from "./islands/Rooms.tsx";
@@ -16,11 +15,10 @@ import * as $$2 from "./islands/Rooms.tsx";
 const manifest = {
   routes: {
     "./routes/[room].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/api/login.ts": $2,
-    "./routes/api/logout.ts": $3,
-    "./routes/index.tsx": $4,
-    "./routes/new.tsx": $5,
+    "./routes/api/login.ts": $1,
+    "./routes/api/logout.ts": $2,
+    "./routes/index.tsx": $3,
+    "./routes/new.tsx": $4,
   },
   islands: {
     "./islands/AddRoom.tsx": $$0,
